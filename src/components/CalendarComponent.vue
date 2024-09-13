d4488eb7308f90c26ae3100db2381f391d072941
--Ax- <andreiamzu@yahoo.com>
2024-09-13 02:44:07 +0200

    Refactor jsconfig.json and package.json

---
+++ src/components/CalendarComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <vue-cal
+      class="vuecal--blue-theme vuecal"
+      :selected-date="'2018-11-19'"
+      :time-from="10 * 60"
+      hide-weekends
+      :events="events"
+      editable-events
+      events-count-on-year-view
+      :events-on-month-view="'short'"
+    />
+  </div>
+</template>
+
+<script>
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
+export default {
+  components: {
+    VueCal,
+  },
+  data() {
+    return {
+      events: [
+        {
+          start: "2018-11-19 10:35",
+          end: "2018-11-19 11:30",
+          title: "Doctor appointment",
+        },
+        {
+          start: "2018-11-19 18:30",
+          end: "2018-11-19 19:15",
+          title: "Dentist appointment",
+        },
+        {
+          start: "2018-11-20 18:30",
+          end: "2018-11-20 20:30",
+          title: "Crossfit",
+        },
+        {
+          start: "2018-11-21 11:00",
+          end: "2018-11-21 13:00",
+          title: "Brunch with Jane",
+        },
+        {
+          start: "2018-11-21 19:30",
+          end: "2018-11-21 23:00",
+          title: "Swimming lesson",
+        },
+        {
+          start: "2019-09-30 19:30",
+          end: "2019-09-30 23:00",
+          title: "Swimming lesson",
+        },
+        {
+          start: "2018-11-19 12:00",
+          end: "2018-11-19 14:00",
+          title: "LUNCH",
+          class: "lunch",
+          background: true,
+        },
+        {
+          start: "2018-11-20 12:00",
+          end: "2018-11-20 14:00",
+          title: "LUNCH",
+          class: "lunch",
+          background: true,
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,38 +1,18 @@
 <template>
-  <div>
-    <vue-cal
-      :events="events"
-      :time-from="8"
-      :time-to="18"
-      default-view="week"
-    />
+  <div class="bg-gray-900 text-white min-h-screen p-4">
+    <div class="container mx-auto space-y-4">
+      <CalendarComponent />
+    </div>
   </div>
 </template>
 
 <script>
-import VueCal from "vue-cal";
-import "vue-cal/dist/vuecal.css";
+import CalendarComponent from "@/components/CalendarComponent.vue";
 
 export default {
   name: "CalendarView",
   components: {
-    VueCal,
-  },
-  data() {
-    return {
-      events: [
-        {
-          start: "2024-09-15 09:00",
-          end: "2024-09-15 11:00",
-          title: "Morning Meeting",
-        },
-        {
-          start: "2024-09-16 14:00",
-          end: "2024-09-16 15:00",
-          title: "Doctor Appointment",
-        },
-      ],
-    };
+    CalendarComponent,
   },
 };
 </script>
